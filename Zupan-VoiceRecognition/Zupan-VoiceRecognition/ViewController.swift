23784a6770a7256e979f59a11ebbe2a59009e093
rodrigo.scroferneker <rodrigo.scroferneker@vodafone.com>
2023-08-15 15:25:10 +0100

    Init project

---
+++ Zupan-VoiceRecognition/Zupan-VoiceRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zupan-VoiceRecognition
+//
+//  Created by Rodrigo Scroferneker on 15/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
