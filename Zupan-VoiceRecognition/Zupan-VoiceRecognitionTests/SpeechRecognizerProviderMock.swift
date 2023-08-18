9eb19d055fad49de3e0205afda40afbc52f39362
rodrigo.scroferneker <rodrigo.scroferneker@vodafone.com>
2023-08-18 16:19:17 +0100

    Add a few tests to SpeechToCommandManager

---
+++ Zupan-VoiceRecognition/Zupan-VoiceRecognitionTests/SpeechRecognizerProviderMock.swift
@@ -0,0 +1,33 @@
+//
+//  SpeechRecognizerProviderMock.swift
+//  Zupan-VoiceRecognitionTests
+//
+//  Created by Rodrigo Scroferneker on 18/08/2023.
+//
+
+import Foundation
+import Combine
+@testable import Zupan_VoiceRecognition
+
+final class SpeechRecognizerProviderMock: SpeechRecognizerProviderType {
+    
+    let mockSequence: [String]
+    private let recognitionSubject = PassthroughSubject<String, SpeechRecognizerError>()
+    
+    init(mockSequence: [String]) {
+        self.mockSequence = mockSequence
+    }
+    
+    var valueReceived: AnyPublisher<String, Zupan_VoiceRecognition.SpeechRecognizerError> {
+        self.mockSequence.forEach { command in self.recognitionSubject.send(command) }
+        return recognitionSubject.eraseToAnyPublisher()
+    }
+    
+    func start() -> AnyPublisher<Void, Zupan_VoiceRecognition.SpeechRecognizerError> {
+        return Empty().eraseToAnyPublisher()
+    }
+    
+    func stop() {
+       
+    }
+}
--- Zupan-VoiceRecognition/Zupan-VoiceRecognitionTests/Zupan_VoiceRecognitionTests.swift
+++ Zupan-VoiceRecognition/Zupan-VoiceRecognitionTests/Zupan_VoiceRecognitionTests.swift
@@ -6,31 +6,87 @@
 //
 
 import XCTest
+import Combine
 @testable import Zupan_VoiceRecognition
 
 final class Zupan_VoiceRecognitionTests: XCTestCase {
+    
+    var stateMachineDescription: StateMachineDescription!
+    var sut: SpeechToCommandManagerType!
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    override func setUp() {
+        super.setUp()
 
+        guard let path = Bundle.main.path(forResource: "stateMachineDescription-en_US", ofType: "json"),
+              let data = try? Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe),
+              let stateMachineDescription = try? JSONDecoder().decode(StateMachineDescription.self, from: data) else {
+            fatalError("Language not available")
+        }
+        
+        self.stateMachineDescription = stateMachineDescription
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testOneCommand() throws {
+        // Given
+        let mockSequence = ["count", "1", "2", "3"]
+        let manager = SpeechRecognizerProviderMock(mockSequence: mockSequence)
+        let sut: SpeechToCommandManagerType = SpeechToCommandManager(manager: manager, stateMachine: self.stateMachineDescription)
+        
+        // When
+        sut.start()
+        manager
+            .valueReceived
+            .sink()
+            .store(in: &cancellables)
+        sut.stop()
+        
+        //Then
+        XCTAssertEqual(sut.history.count, 1)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testBackCommand() throws {
+        // Given
+        let mockSequence = ["count", "1", "2", "3", "code", "3", "count", "3", "back"]
+        let manager = SpeechRecognizerProviderMock(mockSequence: mockSequence)
+        let sut: SpeechToCommandManagerType = SpeechToCommandManager(manager: manager, stateMachine: self.stateMachineDescription)
+        
+        // When
+        sut.start()
+        manager
+            .valueReceived
+            .sink()
+            .store(in: &cancellables)
+        sut.stop()
+        
+        //Then
+        XCTAssertEqual(sut.history.count, 1)
     }
-
+    
+    func testResetCommand() throws {
+        // Given
+        let mockSequence = ["count", "1", "2", "3", "code", "3", "count", "3", "reset"]
+        let manager = SpeechRecognizerProviderMock(mockSequence: mockSequence)
+        let sut: SpeechToCommandManagerType = SpeechToCommandManager(manager: manager, stateMachine: self.stateMachineDescription)
+        
+        // When
+        sut.start()
+        manager
+            .valueReceived
+            .sink()
+            .store(in: &cancellables)
+        sut.stop()
+        
+        //Then
+        XCTAssertEqual(sut.history.count, 2)
+    }
 }
